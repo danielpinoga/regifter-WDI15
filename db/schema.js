8f881254f4f1ec2279824f1901c1df70f232baef
danielpinoga <daniel.pino@generalassemb.ly>
2018-04-09 10:56:55 -0400

    Schema stories coded

--- db/schema.js
+++ db/schema.js
@@ -3,8 +3,22 @@ const Schema = mongoose.Schema
 
 const User = new Schema({
     username: String,
-    text: String,
+    email: String,
     first_name: String,
     last_name: String,
-    photo_url: String
+    photo_url: String,
+    stores: [Store]
+})
+
+const Store = new Schema({
+    name: String,
+    address: String,
+    gifts: [Gift]
+})
+
+const Gift = new Schema({
+    name: String,
+    description: String,
+    price: Number,
+    gift_giver: String
 })
