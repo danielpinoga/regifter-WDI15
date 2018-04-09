87c410a1f55b870decfb81a77042682005adbde5
danielpinoga <daniel.pino@generalassemb.ly>
2018-04-09 15:49:20 -0400

    last commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
