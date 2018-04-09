7d1f6ea4329b4b8bfa8908ed927cb0a4c8dc80ce
danielpinoga <daniel.pino@generalassemb.ly>
2018-04-09 15:13:02 -0400

    Dynamic db link for seeds

--- db/seeds.js
+++ db/seeds.js
@@ -4,7 +4,11 @@ const Gift = require('../models/Gift')
 const mongoose = require('mongoose')
 
 // connect to database
-mongoose.connect("mongodb://localhost/regifter")
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect("mongodb://localhost/regifter")
+}
 
 mongoose.connection.once('open', () => {
     console.log(`Mongoose has connected to MongoDB`)
